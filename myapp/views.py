c63b3639a362d8eed8d4504412899a104c06c83a
nazar1909 <Nazarshylo2005@gmail.com>
2025-10-21 15:10:49 +0300

    celery+redis+rabbit+docker

--- myapp/views.py
+++ myapp/views.py
@@ -13,6 +13,7 @@ from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 from django.contrib.auth.models import User
+from django.core.cache import cache
 
 # Create your views here.
 def home(request):
