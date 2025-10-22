f5f8021dce06e6ea8e2a7caa6df5fc9c6217151f
nazar1909 <Nazarshylo2005@gmail.com>
2025-10-22 18:00:37 +0300

    smth with docker

---
+++ myapp/tasks.py
@@ -0,0 +1,17 @@
+from celery import shared_task
+import time
+
+@shared_task
+def test_task():
+    # Якийсь код вашого старого завдання
+    print("Виконую test_task...")
+    pass
+
+# ДОДАЙТЕ ЦЕЙ КОД В ЦЕЙ Ж ФАЙЛ
+@shared_task
+def add(x, y):
+    print(f"Отримав завдання: {x} + {y}")
+    time.sleep(5)  # Імітуємо 5 секунд роботи
+    result = x + y
+    print(f"Завдання виконано: {result}")
+    return result
