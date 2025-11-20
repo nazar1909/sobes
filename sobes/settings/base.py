f487b324835889779e712a5b4634dbe837e345b6
nazar1909 <Nazarshylo2005@gmail.com>
2025-11-20 23:15:04 +0200

    Fix static files and WS

--- sobes/settings/base.py
+++ sobes/settings/base.py
@@ -84,6 +84,7 @@ SPECTACULAR_SETTINGS = {
 # ======== Middleware ========
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
