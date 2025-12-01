20b8b784f47d589ad8eedad006c71d2e13c1ed60
nazar1909 <Nazarshylo2005@gmail.com>
2025-12-02 00:49:06 +0200

    Fix footer overlap and layout issues

--- sobes/settings/production.py
+++ sobes/settings/production.py
@@ -56,7 +56,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [(REDIS_HOST, REDIS_PORT)], # Використовуємо локальні порти
         },
     },
 }
