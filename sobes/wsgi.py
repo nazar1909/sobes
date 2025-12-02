ccee2952fc306edd35350a9c9e6371b6a3b0aa77
nazar1909 <Nazarshylo2005@gmail.com>
2025-12-02 14:57:46 +0200

    Fix footer overlap and layout issues

--- sobes/wsgi.py
+++ sobes/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sobes.settings.production')
-
+django.setup()
 application = get_wsgi_application()
