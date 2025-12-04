9ec4465637e64ae0b1aeddfd69822e970e801b38
nazar1909 <Nazarshylo2005@gmail.com>
2025-12-04 14:54:31 +0200

    fdsssss

--- myapp/routing.py
+++ myapp/routing.py
@@ -5,9 +5,9 @@ from .consumers import ChatConsumer, NotificationConsumer
 websocket_urlpatterns = [
     # 1. Маршрут для ЧАТУ (для конкретної кімнати)
     # Ми використовуємо (?P<room_name>[^/]+) замість \w+, щоб дозволити більше символів у slug
-    re_path(r'ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>[^/]+)/$', ChatConsumer.as_asgi()),
 
     # 2. 🔥 МАРШРУТ ДЛЯ СПОВІЩЕНЬ (Персональний канал користувача) 🔥
     # Ми використовуємо (?P<user_id>\d+), щоб передати ID користувача в Consumer
-    re_path(r'ws/notifications/(?P<user_id>\d+)/$', consumers.NotificationConsumer.as_asgi()),
+    re_path(r'ws/notifications/(?P<user_id>\d+)/$', NotificationConsumer.as_asgi()),
 ]
